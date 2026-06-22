69a0d717eb6796d46cf4503e360a40e09191099b
亚衣琴 <Saoutax@gmail.com>
2026-06-22 18:23:53 +0800

    feat: add site-lib types

---
+++ src/types/site-lib.d.ts
@@ -0,0 +1,38 @@
+declare function wgUXS(
+    wg: string,
+    hans: string,
+    hant?: string,
+    cn?: string,
+    tw?: string,
+    hk?: string,
+    sg?: string,
+    zh?: string,
+    mo?: string,
+    my?: string,
+): string;
+
+declare const wgULS: typeof wgUXS extends (wg: string, ...rest: infer Rest) => infer ReturnType
+    ? (...args: Rest) => ReturnType
+    : never;
+
+declare const wgUVS: typeof wgULS;
+
+declare function libPrefixNumber(num: number | string, length?: number): string;
+
+type pageNames = {
+    /**
+     * 讨论页名称，若无讨论页则为 false
+     */
+    talkPage: string | false;
+    /**
+     * 当前页面的基础名称（不含名字空间前缀），若当前页面无基础名称则为 false
+     */
+    basePageName: string | false;
+};
+
+/**
+ * 获取当前页面的讨论页名称
+ */
+declare function libGetPageNames(): pageNames;
+
+declare function wgGetEditRequestPreload(pageName?: string, basePageName?: string | false): string;
