7008f48e3ae5911e958fdd95e20f2ca9078e49af
亚衣琴 <Saoutax@gmail.com>
2026-06-29 18:59:48 +0800

    feat: purgeCache

---
+++ dist/gadgets/purgeCache/Gadget-purgeCache.js
@@ -0,0 +1,12 @@
+/**
+ * -------------------------------------------------------------------------
+ * !!! DON'T MODIFY THIS PAGE MANUALLY, YOUR CHANGES WILL BE OVERWRITTEN !!!
+ * !!!     Repository URL: https://github.com/SAPedia/InterfaceCodes     !!!
+ * -------------------------------------------------------------------------
+ */
+
+/* <nowiki> */
+
+"use strict";(()=>{$(()=>{const{wgPageName:e,wgIsArticle:t}=mw.config.get(["wgPageName","wgIsArticle"]);t&&(mw.util.addPortletLink("p-cactions","#","清除缓存","t-purge-cache","点击清除当前页面缓存"),$("#t-purge-cache").on("click",async r=>{r.preventDefault();try{await new mw.Api().post({action:"purge",title:e,forcelinkupdate:!0,forcerecursivelinkupdate:!0}),mw.notify("清除成功，即将刷新……",{type:"success"}),setTimeout(()=>{location.reload()},2e3)}catch(c){mw.notify("清除缓存失败，请重试或于控制台查看详情",{type:"error"}),console.error(`[purgeCache] ${String(c)}`)}}))});})();
+
+/* </nowiki> */
---
+++ src/gadgets/purgeCache/Gadget-purgeCache.ts
@@ -0,0 +1,27 @@
+$(() => {
+    const { wgPageName, wgIsArticle } = mw.config.get(['wgPageName', 'wgIsArticle']);
+
+    if (!wgIsArticle) {
+        return;
+    }
+
+    mw.util.addPortletLink('p-cactions', '#', '清除缓存', 't-purge-cache', '点击清除当前页面缓存');
+    $('#t-purge-cache').on('click', async e => {
+        e.preventDefault();
+        try {
+            await new mw.Api().post({
+                action: 'purge',
+                title: wgPageName,
+                forcelinkupdate: true,
+                forcerecursivelinkupdate: true,
+            });
+            mw.notify('清除成功，即将刷新……', { type: 'success' });
+            setTimeout(() => {
+                location.reload();
+            }, 2000);
+        } catch (err) {
+            mw.notify('清除缓存失败，请重试或于控制台查看详情', { type: 'error' });
+            console.error(`[purgeCache] ${String(err)}`);
+        }
+    });
+});
